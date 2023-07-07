31ec903a095ae47ed4483ad3e8578ce9a2e40fd9
drako9159 <chochito9159@gmail.com>
2023-07-07 14:11:45 -0600

    add complete aplication

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,12 +1,66 @@
-
-
+import {
+  HOST,
+  PAYPAL_API,
+  PAYPAL_API_CLIENT,
+  PAYPAL_API_SECRET,
+} from "../../config.js";
+import axios from "axios";
 
 export async function createOrder(req, res) {
+  const order = {
+    intent: "CAPTURE",
+    purchase_units: [
+      {
+        amount: {
+          currency_code: "USD",
+          value: "100.00",
+        },
+      },
+    ],
+    application_context: {
+      brand_name: "My store",
+      landing_page: "NO_PREFERENCE",
+      user_action: "PAY_NOW",
+      return_url: `${HOST}/capture-order`,
+      cancel_order: `${HOST}/cancel-order`,
+    },
+  };
+  const params = new URLSearchParams();
+  params.append("grant_type", "client_credentials");
+
+  const {
+    data: { access_token },
+  } = await axios.post(`${PAYPAL_API}/v1/oauth2/token`, params, {
+    auth: {
+      username: PAYPAL_API_CLIENT,
+      password: PAYPAL_API_SECRET,
+    },
+  });
+
+  const response = await axios.post(`${PAYPAL_API}/v2/checkout/orders`, order, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  });
+
+  return res.json(response.data);
 }
 
 export async function captureOrder(req, res) {
-  
+  const { token } = req.query;
+  const response = await axios.post(
+    `${PAYPAL_API}/v2/checkout/orders/${token}/capture`,
+    {},
+    {
+      auth: {
+        username: PAYPAL_API_CLIENT,
+        password: PAYPAL_API_SECRET,
+      },
+    }
+  );
+  console.log(response.data);
+  return res.send({ message: "payed" });
 }
-export async function cancelPayment(req, res){
-
+export async function cancelPayment(req, res) {
+  return res.redirect("/");
 }
